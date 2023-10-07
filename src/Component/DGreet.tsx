11ed15e8a1aaa8b8b11d28f8d5c668e47da1c27a
DevPeter <polalekan526@gmail.com>
2023-10-07 10:51:01 +0100

    Work Stated

---
+++ src/Component/DGreet.tsx
@@ -0,0 +1,17 @@
+type DGreetProps = {
+    name: string,
+    isLoggedIn: boolean
+}
+
+const DGreet = (props:DGreetProps) => {
+    return ( 
+        <>
+        <section>
+            { props.isLoggedIn ? (
+            <div className="bg-gray-400 text-2xl font-bold text-white mx-4 py-4">Welcome {props.name}, to your first React and TypeScript Tutorial</div>
+            ) : <div className="bg-gray-400 text-2xl font-bold text-white mx-4 py-4">Welcome Guest</div>}
+            </section>
+        </>
+     );
+}
+export default DGreet;
