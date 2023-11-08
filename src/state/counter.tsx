d3854151ca27d46f852f523b5788f4c8c45de546
DevPeter <polalekan526@gmail.com>
2023-11-08 10:03:21 +0100

    Updated

--- src/state/counter.tsx
+++ src/state/counter.tsx
@@ -3,22 +3,30 @@ type CounterState = {
     count: number
 }
 
-type CounterAction = {
-    type: string
+
+type increDecre = {
+    type: "INCREMENT" | "DECREMENT"
 }
+type Reset = {  
+    type: "RESET"
+}
+type CounterAction = increDecre | Reset
 // Reducer function
+const initialState = { count: 0 };
+
 const reducer = (state: CounterState, action: CounterAction) => {
   switch (action.type) {
     case 'INCREMENT':
       return { count: state.count + 1 };
     case 'DECREMENT':
       return { count: state.count - 1 };
+    case 'RESET':
+    return initialState
     default:
       return state;
   }
 };
 
-const initialState = { count: 0 };
 
 const Counter = () => {
   // Use useReducer
@@ -29,6 +37,8 @@ const Counter = () => {
       <h1>Count: {state.count}</h1>
       <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
       <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
+      <button onClick={() => dispatch({ type: 'RESET' })}>RESET</button>
+
     </div>
   );
 };
